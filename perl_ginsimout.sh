d58f9dc7f6b5c081ce67762e0dfc37b7acbb1d7f
Arnau Montagud <ArnauMontagud@users.noreply.github.com>
2016-04-10 19:00:18 +0200

    Added files via upload

---
+++ perl_ginsimout.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# ./PlMaBoSS_2.0.pl ginsim_out.bnd ginsim_out.cfg threshold=0.1
+# ./PlMaBoSS_2.0.pl ginsim_out.bnd ginsim_outc.cfg 
+./PlMaBoSS_2.0.pl ginsim_out.bnd ginsim_out2.cfg 
