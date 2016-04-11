a1edf174bcb2611b2fc5aefb342011a3a38ce46d
Arnau Montagud <ArnauMontagud@users.noreply.github.com>
2016-04-11 16:21:16 +0200

    Update histogram.R

--- scripts/Robustness analysis/histogram.R
+++ scripts/Robustness analysis/histogram.R
@@ -26,7 +26,6 @@ ggplot(log10, aes(x = Apoptosis)) + geom_histogram(bins = 20, fill="grey", col="
 ggplot(log2, aes(x = Apoptosis)) + geom_histogram(bins = 20, fill="grey", col="white",aes(y = (..count..)/sum(..count..))) + scale_y_continuous(labels=percent) + labs(title = "Phenotype frequency", y = "Percent")
 
 ## Violin plot
-# ratio_2 <- ratio[c(10:73)]
 library(reshape2)
 pheno <- c("Metastasis", "Apoptosis", "nil", "CellCycleArrest")
 ratio_3 <- ratio[pheno]
@@ -40,7 +39,6 @@ log10_2 <- melt(log10[pheno])
 ggplot(log10_2, aes(x=factor(variable), value)) + 
   geom_hline(yintercept=0, colour='red') + 
   geom_violin() +
-#   geom_violin(adjust = .001) + # hi ha menys smoothening
   labs(title = "Phenotype frequencies by log10", y = "mutant/WT log10", x= NULL)
 
 ggplot(log10_2, aes(x=factor(variable), y=value, fill = variable, colour = variable)) + 
@@ -48,14 +46,9 @@ ggplot(log10_2, aes(x=factor(variable), y=value, fill = variable, colour = varia
   geom_violin(alpha=0.7, trim = TRUE, 
               width=.9, adjust = 0.55, # adjust: the bandwidth used is actually adjust*bw. This makes it easy to specify values like ‘half the default’ bandwidth
               scale = "area", # all violins have the same area (before trimming the tails)
-              # width=1, adjust = 1, # adjust: the bandwidth used is actually adjust*bw. This makes it easy to specify values like ‘half the default’ bandwidth
-#             scale = "count", # areas are scaled proportionally to the number of observations
-#             scale = "width", # all violins have the same maximum width
             kernel="rectangular")+ # "gaussian", "rectangular", "triangular", "epanechnikov", "biweight", "cosine" or "optcosine",
-#              kernel="optcosine")+ # "gaussian", "rectangular", "triangular", "epanechnikov", "biweight", "cosine" or "optcosine",
   scale_colour_manual(values = c("orange", "brown", "forestgreen", "blue")) + 
   scale_fill_manual(values = c("orange", "brown", "forestgreen", "blue")) +
-#   theme(panel.grid.minor.x = element_line(colour = "red", linetype = "dotted")) +
   labs(title = "Phenotype frequencies by log10", y = "mutant/WT log10", x= NULL) + 
   theme(legend.position="none")
 
@@ -92,17 +85,3 @@ ggplot() +
   geom_bar(data = dat2, alpha=0.7, aes(x=variable, y=-value/sum(value)),stat = "identity", fill='brown') + 
   scale_y_continuous(labels=percent) + 
   labs(title = "Phenotype frequency", y = "log10 percent", x= NULL)
-
-# hist (as.matrix(a), breaks = 100)
-# hist (as.matrix(m))
-# hist (as.matrix(ratio$Metastasis))
-# hist (as.matrix(log2$Metastasis))
-# hist (as.matrix(log10$Metastasis))
-# hist (as.matrix(log10$Metastasis),freq=FALSE) # total area of hist is 1
-# with percentages:
-# h = hist (as.matrix(log10$Metastasis))
-# h = hist(x)
-# h$density = h$counts/sum(h$counts)*100
-# plot(h,freq=FALSE,ylab='Percentage')
-# also :
-# ggplot(log10, aes(x = Metastasis)) + geom_bar(aes(y = (..count..)/sum(..count..))) + scale_y_continuous(labels=percent) + labs(title = "Phenotype frequency", y = "Percent")
