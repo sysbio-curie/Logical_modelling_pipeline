55ccf0c1b63e5171b705c32b58bb42794c5987e5
Arnau Montagud <arnau.montagud@gmail.com>
2017-10-23 11:39:21 +0200

    corrected Analyses logical gates

--- scripts/run all analyses/7 Analyses of logical gates/Analyses_of_logical_gates_Stew.R
+++ scripts/run all analyses/7 Analyses of logical gates/Analyses_of_logical_gates_Stew.R
@@ -39,16 +39,11 @@ ggplot(b4, aes(y = COUNT,x=Dist_to_WT)) + geom_point() + scale_x_continuous(brea
 
 ## unique stable states:
 ggplot(b4, aes(x = Dist_to_WT)) + geom_histogram(stat="count",bins = 30, col="white") + labs(x = "Distance to closest WT stable state of unique stable states",y = "Counts")+ theme(panel.grid.minor = element_blank()) + scale_x_continuous(breaks=seq(0,12,1))
-# ggplot(b4, aes(x = Dist_to_WT)) + geom_histogram(stat="count",bins = 30, col="white",aes(y = (..count..)/sum(..count..))) + scale_y_continuous(labels=percent) + scale_x_continuous(breaks=seq(0,12,1)) + labs(x = "Distance to closest WT stable state of unique stable states",y = "Percentage")+ theme(panel.grid.minor = element_blank())
-# uniqueSSsum<-as.data.frame(table(b4$Dist_to_WT))
-# uniqueSSsum$Percentage <- round(uniqueSSsum$Freq / sum(uniqueSSsum$Freq) * 100,4)
-# uniqueSSsum
 
 ## looking at the WT stable states by closest to mutants stable states
 b5<-as.data.frame(table(b3$Closest_WT))
 b5<-b5[-c(nrow(b5)),]
 ggplot(b5, aes(Var1,Freq)) + geom_bar(stat = "identity") + labs(x = "WT stable states",y = "Counts")
-# ggplot(b5, aes(Var1,Freq)) + geom_bar(stat="identity",col="white",aes(y = (b5$Freq)/sum(b5$Freq))) + scale_y_continuous(labels=percent) + labs(x = "WT stable states",y = "Percentage")
 dev.off()
 
 # Histogram on genetic interactions ----
@@ -60,9 +55,6 @@ colnames(a4)<-gsub(".","-",colnames(a4),fixed = T)
 WT<-a4[which(rownames(a4)=="WT"),]
 ratio<-scale(as.matrix(a4), center=FALSE, scale=as.matrix(WT))
 ratio<-as.data.frame(ratio[order(rownames(ratio)), ])
-# ratio_1<-scale(as.matrix(a4+1), center=FALSE, scale=as.matrix(WT+1))
-# ratio_1<-as.data.frame(ratio_1[order(rownames(ratio_1)), ])
-# resta<- as.data.frame(sweep(as.matrix(a4),2,as.matrix(WT)))
 
 ## draw histograms of ratio (mutant / WT)
 pdf("Logical_gates_ratio_histograms_phenotypes_most_probable.pdf",onefile=T)
@@ -72,7 +64,6 @@ for (i in c5){
   col <- rep("grey", 30)
   col[findInterval((ratio[(which(rownames(ratio)=="WT")),])[,i], ag$data[[1]]$xmin)]<-"dark red"
   print(ggplot(ratio, aes(x = ratio[,i])) + geom_histogram(bins = 30, fill=col, col="white",aes(y = (..count..)/sum(..count..))) + scale_y_continuous(labels=percent,breaks = pretty_breaks(10)) + scale_x_continuous(labels=comma,breaks = pretty_breaks(10)) +labs(x = paste0(i," phenotype ratio logical gate mutant / WT"), y = "Percentage"))
-        # print(ggplot(ratio, aes(x = ratio[,i])) + geom_histogram(bins = 30, fill=col, col="white",aes(y = (..count..)/sum(..count..))) + scale_y_continuous(labels=percent,breaks = pretty_breaks(10)) + scale_x_continuous(labels=comma,breaks = pretty_breaks(10)) +labs(x = paste0(i," phenotype ratio mutant / WT"), y = "Percentage"))
 }
 dev.off()
 
