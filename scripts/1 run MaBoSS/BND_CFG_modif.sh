285b37f0f6075b0f952cf3f7bc9ea25d4c22a604
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2017-12-21 19:18:50 +0100

    In-place sed for both GNU sed and BSD/macOS sed
    For GNU sed, the suffix for the -i option is optional.
    But OS X ships with BSD sed, where the suffix is mandatory.
    
    The trick proposed here make the command works with both implementations.
    It would not work with -i'' only, because to sed it is indistinguishable from
    just -i, because the shell effectively removes the empty quotes and passes just
    -i.
    
    With (effectively) just -i specified, it is the next argument that is
    interpreted as the option-argument
    
    Note :
    it is not portable in a POSIX sense, because the -i option is not
    POSIX-compliant.

--- scripts/1 run MaBoSS/BND_CFG_modif.sh
+++ scripts/1 run MaBoSS/BND_CFG_modif.sh
@@ -2,7 +2,7 @@
 
 # - modifying CFG file
 # Define outputs in file called "listOut" (needed for _ko and _up parameters of epistasis) and inputs in file called "listIn" (needed for is_internal parameters of MaBoSS), one output per line
-sed -i 's/ =/=/g;s/= /=/g;s/ ;/;/g' ginsimout.cfg
+sed -i.temp 's/ =/=/g;s/= /=/g;s/ ;/;/g' ginsimout.cfg
 
 states=$(grep Node ginsimout.bnd | sed 's/Node //g;s/ {//g')
 inputs=$(sed ':a;N;$!ba;s/\n/ /g;s/^/ /g' listIn.txt)
@@ -22,7 +22,7 @@ unset IFS
 for input in $inputs; do
 		echo "s:^"$input"\.istate://"$input"\.istate:g" >>sedfile.temp
 done
-sed -i -f sedfile.temp ginsimout.cfg
+sed -i.temp -f sedfile.temp ginsimout.cfg
 
 # adding inputs as internal and outputs as NOT internal
 # inputs are internal so that they are not considered in probtraj file headers
@@ -35,13 +35,12 @@ for state in $states; do
 		echo $state | sed "s/$/\.is_internal=1;/" >> internal.temp
 	fi
 done
-sed -i '1i\\' internal.temp
+sed -i.temp '1i\\' internal.temp
+
 
 cat internal.temp >> ginsimout.cfg
 
-rm *.temp
-
-sed -i '
+sed -i.temp '
 s/sample_count = 500000/sample_count = 50000/;
 s/time_tick = 0.01/time_tick = 0.1/;
 s/max_time = 5/max_time = 30/;
@@ -49,7 +48,7 @@ s/max_time = 5/max_time = 30/;
 
 # some changes to adjust GinSim export to the file used in Cohen et al, PLoS Comp Biol, 2015; doi:10.1371/journal.pcbi.1004571.g003
 # cp ginsimout.cfg ginsimout_Cohen.cfg
-# sed -i '
+# sed -i.temp '
 # s/sample_count = 500000/sample_count = 50000/;
 # s/max_time = 5/max_time = 30/;
 # s/time_tick = 0.01/time_tick = 0.1/;
@@ -63,9 +62,13 @@ cp ginsimout.bnd ginsimout_original.bnd
 names=$(grep Node ginsimout.bnd | sed 's/Node //;s/ {//')
 for name in $names; do
 if [[ $outputs != *"$name"* ]]; then
-sed -i "
+sed -i.temp "
 s/rate_up = @logic ? \$u_"$name" : 0;/rate_up  = \( \$"$name"_ko ? 0.0 : \( \$"$name"_up ? @max_rate : \( @logic ? \$u_"$name" : 0)));/;
 s/rate_down = @logic ? 0 : \$d_"$name";/rate_down  = ( \$"$name"_ko ? @max_rate : ( \$"$name"_up ? 0.0 : ( @logic ? 0 : \$d_"$name")));\\
  \t max_rate = 1.7976931348623157E+308\/1;/" ginsimout.bnd
 fi
 done
+
+
+
+rm *.temp
